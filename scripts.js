d2712f4c66e6fe8214b79e5106a1e392ccd205a9
calebehler <calebehler99@gmail.com>
2022-05-09 20:51:12 -0300

    Fixed API call

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
 var request = new XMLHttpRequest()
 
-request.open('GET', 'http://api.weatherapi.com/v1//current.json?key=<fda88d0772da44f08d0124849220905>&q=Halifax', true)
+request.open('GET', 'http://api.weatherapi.com/v1/current.json?key=fda88d0772da44f08d0124849220905&q=London&aqi=no', true)
 
 request.onload = function(){
   
   //access JSON data here
   var data = JSON.parse(this.response)
   
-  data.forEach(temp_c => {
-    //log temp for testing
-    console.log(temp_c)
-  })
+  console.log(data)
   
 }
 
