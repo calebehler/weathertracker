7b6ac6d0e9556c76dea09781226323fc43b255a5
calebehler <103388662+calebehler@users.noreply.github.com>
2022-05-09 11:04:42 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+var request = new XMLHttpRequest()
 
+request.open('GET', 'http://api.weatherapi.com/v1//current.json', true)
+
+request.onload = function(){
+  
+  //access JSON data here
+  var data = JSON.parse(this.response)
+  
+  data.forEach(temp_c => {
+    //log temp for testing
+    console.log(temp_c)
+  })
+  
+}
+
+//send request
+request.send()
