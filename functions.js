7e2eb4528f262b196a294d5957310fcc4c083cc1
calebehler <calebehler99@gmail.com>
2022-05-09 21:47:30 -0300

    Added a button lol

---
+++ functions.js
@@ -0,0 +1,11 @@
+function updateData() {
+    request.open('GET', 'http://api.weatherapi.com/v1/current.json?key=fda88d0772da44f08d0124849220905&q=London&aqi=no', true)
+    request.onload = function () {
+        //access JSON data here
+        var data = JSON.parse(this.response)
+
+        console.log(data)
+    }
+//send request
+    request.send()
+}
--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
 var request = new XMLHttpRequest()
 
 request.open('GET', 'http://api.weatherapi.com/v1/current.json?key=fda88d0772da44f08d0124849220905&q=London&aqi=no', true)
-
-request.onload = function(){
-  
+request.onload = function () {
   //access JSON data here
-  var data = JSON.parse(this.response)
-  
-  console.log(data)
-  
-}
+    var data = JSON.parse(this.response)
 
+    console.log(data)
+  }
 //send request
-request.send()
+  request.send()
+
+data.forEach(current)
+
+
+
+
